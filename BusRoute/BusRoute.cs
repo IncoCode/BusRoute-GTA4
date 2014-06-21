b6a101083ae8156ee2f286035c954d62b883347e
IncoCode <akulich.dimon@gmail.com>
2014-06-21 14:42:51 +0300

    clean

--- BusRoute/BusRoute.cs
+++ BusRoute/BusRoute.cs
@@ -2,8 +2,6 @@
 
 using System;
 using System.Collections.Generic;
-using System.Threading;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using GTA;
 
@@ -18,7 +16,7 @@ namespace BusRoute
             private readonly Vehicle _bus;
             private Ped _driver;
             private readonly List<Ped> _passagers;
-            private Vector3 _dest;
+            private Ped _passagerDest;
             private Blip _blip;
 
             public Vehicle BusVehicle
@@ -31,13 +29,26 @@ namespace BusRoute
                 get { return this._driver; }
             }
 
-            public Bus( Vector3 spawnPos )
+            public List<Ped> Passagers
+            {
+                get { return this._passagers; }
+            }
+
+            public Ped Destination
+            {
+                get { return this._passagerDest; }
+            }
+
+            public bool Driving { get; set; }
+
+            public Bus( Vector3 spawnPos, Vector3 _dest )
                 : this()
             {
-                this._bus = World.CreateVehicle( "BUS", spawnPos );
-                //this._bus.RotationQuaternion = new Quaternion();
-                this._bus.Rotation = new Vector3( 15, 0, 195 );
+                this._bus = World.CreateVehicle( "BANSHEE", spawnPos );
+                this._bus.Rotation = new Vector3( 0, 0, 180 );
+                this._driver = this._bus.CreatePedOnSeat( VehicleSeat.Driver );
                 this._passagers = new List<Ped>();
+                this._passagerDest = World.CreatePed( _dest );
             }
 
             public Bus()
@@ -48,18 +59,6 @@ namespace BusRoute
             {
                 Random random = new Random();
                 return random.Next( 1, 100 ) >= 50 ? Gender.Male : Gender.Female;
-            }
-
-            /// <summary>
-            /// Создает водителя для автобуса
-            /// </summary>
-            public Ped CreateDriver()
-            {
-                this._driver = World.CreatePed( this._bus.Position.Around( 10F ), Gender.Male );
-                var tasks = new TaskSequence();
-                tasks.AddTask.EnterVehicle( this._bus, VehicleSeat.Driver );
-                this._driver.Task.PerformSequence( tasks );
-                return this._driver;
             }
 
             /// <summary>
@@ -74,33 +73,16 @@ namespace BusRoute
                 this._passagers.Add( passager );
                 return passager;
             }
-
-            public void DriveRoute( Vector3 targetPoint )
-            {
-                var tasks = new TaskSequence();
-                this._dest = targetPoint;
-                tasks.AddTask.DriveTo( this._dest, 25, true, true );
-                this._driver.Task.PerformSequence( tasks );
-                //while ( !this._driver.Position.Equals( _dest.Around( 3F ) ) )
-                //{
-                //    this.Wait( 1500 );
-                //}
-                //this._driver.NoLongerNeeded();
-                //this._bus.NoLongerNeeded();
-                ////this._checkpoint.Disable();
-                //Array.ForEach( this._passagers.ToArray(), p => p.NoLongerNeeded() );
-                //Game.DisplayText( "Done" );
-            }
         }
 
         //private readonly Vector3 _busSpawnVector = new Vector3( 1039.08F, 280.66F, 30.45F );
-        private readonly Vector3 _busSpawnVector = new Vector3( 1039.08F, 280.66F, 30.45F );
+        private readonly Vector3 _busSpawnVector = new Vector3( 1087.16F, 197.99F, 30.72F );
         private readonly List<Bus> _busses;
         private readonly Dictionary<Bus, Blip> _blips;
 
         public BusRoute()
         {
-            this.Interval = 100;
+            this.Interval = 50;
             this.Tick += this.BusRoute_Tick;
             this.KeyDown += new GTA.KeyEventHandler( this.BasicKeyExample_KeyDown );
             this._busses = new List<Bus>();
@@ -109,11 +91,22 @@ namespace BusRoute
 
         private void BusRoute_Tick( object sender, EventArgs e )
         {
+            foreach ( Bus bus in this._busses )
+            {
+                if ( !bus.Driver.isSittingInVehicle( bus.BusVehicle ) )
+                {
+                    continue;
+                }
+                bus.Driver.Task.ClearAll();
+                bus.Driver.Task.AlwaysKeepTask = true;
+                //bus.Driver.Task.DriveTo( bus.BusVehicle, World.GetNextPositionOnStreet( bus.Destination ), 35, false, true );
+                bus.Driver.Task.DriveTo( bus.BusVehicle, bus.Destination, 35, true, true );
+            }
         }
 
         private void BasicKeyExample_KeyDown( object sender, GTA.KeyEventArgs e )
         {
-            if ( e.Key == Keys.F5 )
+            if ( e.Key == Keys.F6 )
             {
                 this.SpawnBusWithDriver();
             }
@@ -123,39 +116,20 @@ namespace BusRoute
         {
             if ( !ped.isInVehicle( vehicle ) )
             {
-                Game.DisplayText( "No in Veh" );
-                this.Wait( 1000 );
-            }
-        }
-
-        private void CreateDriver( Bus bus )
-        {
-            Ped driver = bus.CreateDriver();
-            this.WaitWhilePedNotInCar( driver, bus.BusVehicle );
-        }
-
-        private void CreatePassagers( Bus bus, int passagersCount )
-        {
-            for ( int i = 0; i < passagersCount; i++ )
-            {
-                Ped passager = bus.CreatePassager();
-                this.WaitWhilePedNotInCar( passager, bus.BusVehicle );
+                this.Wait( 1 );
             }
         }
 
         private void SpawnBusWithDriver()
         {
-            Bus bus = new Bus( this._busSpawnVector );
+            Bus bus = new Bus( this._busSpawnVector,
+                new Vector3( this.Player.Character.Position.X, this.Player.Character.Position.Y,
+                    this.Player.Character.Position.Z ) );
             this._busses.Add( bus );
-            this.Wait( 3500 );
-            this.CreateDriver( bus );
-            this.Wait( 10500 );
-            this.CreatePassagers( bus, 1 );
             Blip blip = Blip.AddBlip( bus.BusVehicle );
-            blip.Color = BlipColor.DarkRed;
+            blip.Color = BlipColor.DarkTurquoise;
             blip.Name = "Bus";
             this._blips.Add( bus, blip );
-            bus.DriveRoute( this.Player.Character.Position );
         }
     }
 }
